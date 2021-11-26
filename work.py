964fa231726114d81318a5eb8615cec2a7703cec
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-11-26 14:22:53 +0900

    readme.md updated

--- work.py
+++ work.py
@@ -1,52 +1,101 @@
-# def convert2input(self, input, output):
-#     """
-#    '1 + 3 = * 4 =' の場合
-#        eval        pyautogui
-#     1   1           typewrite(1)
-#     +   1 + click('+')
-#     3   1 + 3       typewrite(3)
-#     =   r = eval(1 + 3) click('=')
-#     *   r * click(*)
-#     4   r * 4       typewrite(4)
-#     =   r = eval(r * 4) click('=')
-
-#     """
-#     result = 0
-#      target = ''
-
-#       input_list = input.split()
-#        for i in input_list:
-#             # if i not in ['+', '-', '*', '/', '=', '%']:
-#             #     print(f'type {i}')
-#             # else:
-#             #     print(f'click {i}')
-
-#             if i == '=':
-#                 # calc.typewrite(str(target), f'{target}')
-#                 calc.click('equal', '= was typed')
-#                 result = eval(target)
-#                 target = str(result)
-#             elif i in self.calcchar.keys():
-#                 calc.click(self.calcchar[i], f'click {i}')
-#                 target = target + i
-#             else:
-#                 calc.typewrite(i, f'{i} was typed')
-#                 target = target + i
-#         self.confirm(f'{input} : eval result {result} and result {output}')
-
-def funcA(secondarg=''):
-    print('funcA', secondarg)
+"""
+https://dev.to/karn/building-a-simple-state-machine-in-python
+"""
 
 
-def funcB(secondarg=''):
-    print('funcB', secondarg)
+class State(object):
+    """
+    We define a state object which provides some utility functions for the
+    individual states within the state machine.
+    """
+
+    def __init__(self):
+        print(f'Processing current state:, {str(self)}')
+
+    def on_event(self, event):
+        """
+        Handle events that are delegated to this State.
+        """
+        pass
+
+    def __repr__(self):
+        """
+        Leverages the __str__ method to describe the State.
+        """
+        return self.__str__()
+
+    def __str__(self):
+        """
+        Returns the name of the State.
+        """
+        return self.__class__.__name__
 
 
-def func_caller(func, secondarg=''):
-    print(f'func_caller called {func=}')
-    func(secondarg)
-    print('func_caller ended')
+# Start of our states
+"""
+ステートは以下の3つ
+waiting_state
+proc1_state
+proc2_state
+
+eventは4つ
+input_num
+input_op
+input_equal
+input_clear
+input_sp_op
+"""
 
 
-func_caller(funcA)
-func_caller(funcA, 'original message')
+class WaitingState(State):
+
+    def on_event(self, event):
+
+
+class LockedState(State):
+    """
+    The state which indicates that there are limited device capabilities.
+    """
+
+    def on_event(self, event):
+        if event == 'pin_entered':
+            return UnlockedState()
+
+        return self
+
+
+class UnlockedState(State):
+    """
+    The state which indicates that there are no limitations on device
+    capabilities.
+    """
+
+    def on_event(self, event):
+        if event == 'device_locked':
+            return LockedState()
+
+        return self
+# End of our states.
+
+
+class SimpleDevice(object):
+    """
+    A simple state machine that mimics the functionality of a device from a
+    high level.
+    """
+
+    def __init__(self):
+        """ Initialize the components. """
+
+        # Start with a default state.
+        self.state = LockedState()
+
+    def on_event(self, event):
+        """
+        This is the bread and butter of the state machine. Incoming events are
+        delegated to the given states which then handle the event. The result is
+        then assigned as the new state.
+        """
+
+        # The next state will be the result of the on_event function.
+        self.state = self.state.on_event(event)
